f19a8d8a744f9805a4d226ea65a0ab2c7cb775b4
verdantcap <alex3621lin@gmail.com>
2024-12-03 11:18:08 +0300

    feat: fixed hello.py

--- src/dags/demo_dags/hello.py
+++ src/dags/demo_dags/hello.py
@@ -3,6 +3,9 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 
+import boto3
+from botocore.exceptions import NoCredentialsError
+
 # Define default arguments
 default_args = {
     'owner': 'airflow',
@@ -14,6 +17,24 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+def list_s3_files(bucket_name: str):
+    """
+    List all files in the specified S3 bucket.
+    """
+    # Create a session using the default configuration
+    # Boto3 will automatically use the credentials provided by IRSA
+    session = boto3.Session()
+    s3_client = session.client('s3')
+    
+    paginator = s3_client.get_paginator('list_objects_v2')
+    
+    try:
+        for page in paginator.paginate(Bucket=bucket_name):
+            for obj in page.get('Contents', []):
+                print(obj['Key'])
+    except NoCredentialsError:
+        print("Credentials not available.")
+
 # Instantiate the DAG
 dag = DAG(
     'hello_world',
@@ -24,7 +45,11 @@ dag = DAG(
 
 # Define the Python function to be executed
 def print_hello():
-    return 'Hello World from Airflow!'
+    bucket_name = "airflow-storage-dev-us-east-2-genie-platforms"
+    
+    # List files in the S3 bucket
+    list_s3_files(bucket_name)
+
 
 # Create tasks
 start_task = EmptyOperator(task_id='start_task', dag=dag)
