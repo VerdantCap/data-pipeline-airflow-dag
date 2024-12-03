40b1c1d4e6db9b2bf05f0bf12a9b9dd497877933
verdantcap <alex3621lin@gmail.com>
2024-12-03 11:48:23 +0300

    feat: add the test case

--- src/dags/demo_dags/hello.py
+++ src/dags/demo_dags/hello.py
@@ -39,6 +39,25 @@ def list_s3_files(bucket_name: str, prefix: str = ''):
     except NoCredentialsError:  
         print("Credentials not available.") 
 
+def write_string_to_s3(bucket_name: str, s3_key: str, content: str):  
+    """  
+    Write a string into a file and upload it to the specified S3 bucket.  
+
+    :param bucket_name: Name of the S3 bucket.  
+    :param s3_key: S3 object key where the string is stored in the bucket.  
+    :param content: String content to write to the file in S3.  
+    """  
+    session = boto3.Session()  
+    s3_client = session.client('s3')  
+    
+    try:  
+        s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=content)  
+        print(f"Content uploaded to {bucket_name}/{s3_key}")  
+    except NoCredentialsError:  
+        print("Credentials not available.")  
+    except Exception as e:  
+        print(f"An error occurred: {str(e)}") 
+
 # Instantiate the DAG
 dag = DAG(
     'hello_world',
@@ -50,9 +69,13 @@ dag = DAG(
 # Define the Python function to be executed
 def print_hello():
     bucket_name = "airflow-storage-dev-us-east-2-genie-platforms"
-    prefix = 'airflow/logs/dag_id=hello_world'  # Change this to your subdirectory  
+    prefix = 'airflow/logs/1.txt'  # Change this to your subdirectory  
     # List files in the S3 bucket subdirectory  
-    list_s3_files(bucket_name, prefix)
+
+    content = "This is the string content to be stored in the file."  # The string content you want to write  
+
+    # Write the string as a file to the S3 bucket  
+    write_string_to_s3(bucket_name, prefix, content)
 
 
 # Create tasks
