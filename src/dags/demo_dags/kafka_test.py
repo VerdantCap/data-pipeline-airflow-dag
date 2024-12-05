a9dd07a290ba3bbc16df2513e66d63a2884954ce
verdantcap <alex3621lin@gmail.com>
2024-12-05 10:37:25 +0300

    feat: fixed err_21

--- src/dags/demo_dags/kafka_test.py
+++ src/dags/demo_dags/kafka_test.py
@@ -1,20 +1,20 @@
-from airflow import DAG
-from datetime import datetime, timedelta
-from airflow.providers.apache.kafka.operator.consume import ConsumeFromTopicOperator
-def func(message, prefix=None):
-    print(message)
+# from airflow import DAG
+# from datetime import datetime, timedelta
+# from airflow.providers.apache.kafka.operator.consume import ConsumeFromTopicOperator
+# def func(message, prefix=None):
+#     print(message)
 
-with DAG(dag_id="test_kafka",
-         start_date=datetime(2021,1,1),
-         schedule_interval='*/2 * * * *'
-         ) as dag:
-         get_messages=ConsumeFromTopicOperator(
-          task_id="get_messages",
-          topics=["topictest"],
-          apply_function='test_kafka.func',
-          consumer_config = {
-            'group.id':'test-consumer-group',
-            'bootstrap.servers': 'server:9092',
-            "auto.offset.reset": "earliest",})
+# with DAG(dag_id="test_kafka",
+#          start_date=datetime(2021,1,1),
+#          schedule_interval='*/2 * * * *'
+#          ) as dag:
+#          get_messages=ConsumeFromTopicOperator(
+#           task_id="get_messages",
+#           topics=["topictest"],
+#           apply_function='test_kafka.func',
+#           consumer_config = {
+#             'group.id':'test-consumer-group',
+#             'bootstrap.servers': 'server:9092',
+#             "auto.offset.reset": "earliest",})
 
-get_messages
+# get_messages
